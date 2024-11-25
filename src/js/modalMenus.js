b7c116eac978f08ac474d251996c627ef1135c35
greyskin <hissra6@gmail.com>
2024-11-25 19:21:10 +0300

    initial commit

--- src/js/modalMenus.js
+++ src/js/modalMenus.js
@@ -1,83 +1,83 @@
-const burgerBtn = document.querySelector(".upper-menu__button-burger");
-const burgerMenu = document.querySelector(".burger-menu");
-const burgerMenuContainer = document.querySelector(".burger-menu__container");
-const closeBtn = document.querySelector(".head__close-menu");
+const burgerBtn = document.querySelector('.round-icon--burger')
+const burgerMenu = document.querySelector('.burger-menu')
+const burgerMenuContainer = document.querySelector('.burger-menu__container')
+const closeBtn = document.querySelector('.round-icon--close-menu')
 
-burgerBtn.addEventListener("click", displayMenu);
-closeBtn.addEventListener("click", closeMenu);
-burgerMenu.addEventListener("click", (c) => {
-  const withinBoundaries = c.composedPath().includes(burgerMenuContainer);
+burgerBtn.addEventListener('click', displayMenu)
+closeBtn.addEventListener('click', closeMenu)
+burgerMenu.addEventListener('click', (c) => {
+  const withinBoundaries = c.composedPath().includes(burgerMenuContainer)
   if (!withinBoundaries) {
-    burgerMenu.style.display = "none";
+    burgerMenu.style.display = 'none'
   }
-});
+})
 
 function displayMenu() {
-  burgerMenu.style.display = "block";
-  burgerMenuContainer.classList.add("active");
+  burgerMenu.style.display = 'block'
+  burgerMenuContainer.classList.add('burger-menu__container--active')
 }
 
 function closeMenu() {
-  burgerMenu.style.display = "none";
-  burgerMenuContainer.classList.remove("active");
+  burgerMenu.style.display = 'none'
+  burgerMenuContainer.classList.remove('burger-menu__container--active')
 }
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   if (window.innerWidth >= 1120) {
-    displayMenu();
+    displayMenu()
   } else if (window.innerWidth < 1120) {
-    closeMenu();
+    closeMenu()
   }
-});
+})
 
-const feedbackBtn = document.querySelector(".contact__chat");
-const feedbackBtn2 = document.querySelector(".upper-menu__button-chat");
-const feedback = document.querySelector(".feedback");
-const feedbackContainer = document.querySelector(".feedback__container");
-const closeBtnFeedback = document.querySelector(".feedback__close-menu");
+const feedbackBtn = document.querySelector('.round-icon--chat-burger')
+const feedbackBtn2 = document.querySelector('.round-icon--chat')
+const feedback = document.querySelector('.feedback')
+const feedbackContainer = document.querySelector('.feedback__container')
+const closeBtnFeedback = document.querySelector('.round-icon--close-menu')
 
-feedbackBtn.addEventListener("click", displayMenuFeedback);
-feedbackBtn2.addEventListener("click", displayMenuFeedback);
-closeBtnFeedback.addEventListener("click", closeMenuFeedback);
-feedback.addEventListener("click", (c) => {
-  const withinBoundaries = c.composedPath().includes(feedbackContainer);
+feedbackBtn.addEventListener('click', displayMenuFeedback)
+feedbackBtn2.addEventListener('click', displayMenuFeedback)
+closeBtnFeedback.addEventListener('click', closeMenuFeedback)
+feedback.addEventListener('click', (c) => {
+  const withinBoundaries = c.composedPath().includes(feedbackContainer)
   if (!withinBoundaries) {
-    feedback.style.display = "none";
+    feedback.style.display = 'none'
   }
-});
+})
 
 function displayMenuFeedback() {
-  feedback.style.display = "block";
-  feedbackContainer.classList.add("active");
+  feedback.style.display = 'flex'
+  feedbackContainer.classList.add('feedback__container--active')
 }
 
 function closeMenuFeedback() {
-  feedback.style.display = "none";
-  feedbackContainer.classList.remove("active");
+  feedback.style.display = 'none'
+  feedbackContainer.classList.remove('feedback__container--active')
 }
 
-const requestCallBtn = document.querySelector(".contact__call");
-const requestCallBtn2 = document.querySelector(".upper-menu__button-call");
-const requestCall = document.querySelector(".request-call");
-const requestCallContainer = document.querySelector(".request-call__container");
-const closeBtnRequestCall = document.querySelector(".request-call__close-menu");
+const requestCallBtn = document.querySelector('.round-icon--call-burger')
+const requestCallBtn2 = document.querySelector('.round-icon--call')
+const requestCall = document.querySelector('.request-call')
+const requestCallContainer = document.querySelector('.request-call__container')
+const closeBtnRequestCall = document.querySelector('.round-icon--close-menu')
 
-requestCallBtn.addEventListener("click", displayMenuRequestCall);
-requestCallBtn2.addEventListener("click", displayMenuRequestCall);
-closeBtnRequestCall.addEventListener("click", closeMenuRequestCall);
-requestCall.addEventListener("click", (c) => {
-  const withinBoundaries = c.composedPath().includes(requestCallContainer);
+requestCallBtn.addEventListener('click', displayMenuRequestCall)
+requestCallBtn2.addEventListener('click', displayMenuRequestCall)
+closeBtnRequestCall.addEventListener('click', closeMenuRequestCall)
+requestCall.addEventListener('click', (c) => {
+  const withinBoundaries = c.composedPath().includes(requestCallContainer)
   if (!withinBoundaries) {
-    requestCall.style.display = "none";
+    requestCall.style.display = 'none'
   }
-});
+})
 
 function displayMenuRequestCall() {
-  requestCall.style.display = "block";
-  requestCallContainer.classList.add("active");
+  requestCall.style.display = 'flex'
+  requestCallContainer.classList.add('request-call__container--active')
 }
 
 function closeMenuRequestCall() {
-  requestCall.style.display = "none";
-  requestCallContainer.classList.remove("active");
+  requestCall.style.display = 'none'
+  requestCallContainer.classList.remove('request-call__container--active')
 }
